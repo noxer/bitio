f3b70a061fdac1ead02c00554aad7f0d3ed085af
Tim Scheuermann <tim.scheuermann@gmail.com>
2019-10-28 19:52:29 +0100

    Initial implementation, untested

---
+++ reader.go
@@ -0,0 +1,125 @@
+package bitio
+
+import "io"
+
+// Reader offers support for reads of arbitrary bit length.
+type Reader struct {
+	len  uint
+	r    io.Reader
+	bits byte
+}
+
+// NewReader creates a new bit reader for r. If r is already a bit reader it just returns r.
+func NewReader(r io.Reader) *Reader {
+	if br, ok := r.(*Reader); ok {
+		return br
+	}
+	return &Reader{
+		r: r,
+	}
+}
+
+// fillBits makes sure that at least one bit is in the bits buffer, otherwise it will try to read a new byte into the buffer.
+func (r *Reader) fillBits() error {
+	if r.len != 0 {
+		return nil
+	}
+
+	var buf [1]byte
+	_, err := r.r.Read(buf[:])
+	if err != nil {
+		return err
+	}
+
+	r.bits = buf[0]
+	r.len = 8
+
+	return nil
+}
+
+// ReadBits8 reads n bits from the source into the returned byte. Will not read more than 8 bits.
+func (r *Reader) ReadBits8(n uint) (uint8, error) {
+	if n > 8 {
+		n = 8
+	}
+
+	// make sure the buffer is filled
+	if err := r.fillBits(); err != nil {
+		return 0, err
+	}
+
+	// fast path, we've got exactly the amount of bits needed to fulfill the request
+	if r.len == n {
+		r.len = 0
+		return r.bits, nil
+	}
+
+	// make sure we're not trying to read too many bits
+	var rem uint
+	if n > r.len {
+		rem = n - r.len
+		n = r.len
+	}
+
+	r.len -= n
+	res := r.bits >> r.len
+	r.bits &= 1<<r.len - 1
+
+	// we may be done here
+	if rem == 0 {
+		return res, nil
+	}
+
+	// shift the partial result and read the other part
+	res <<= rem
+	o, err := r.ReadBits8(rem)
+	return res | o, err
+}
+
+func (r *Reader) Read(p []byte) (int, error) {
+	var (
+		i   = -1
+		err error
+	)
+	for i = range p {
+		p[i], err = r.ReadBits8(8)
+		if err != nil {
+			break
+		}
+	}
+
+	return i + 1, err
+}
+
+// ReadBits reads n bits into p. The function will not read more bits that can be contained in p.
+func (r *Reader) ReadBits(p []byte, n uint) (int, error) {
+	// make sure to only read the appropriate number of bits
+	if maxLen := uint(len(p)) * 8; maxLen < n {
+		n = maxLen
+	}
+
+	// fill the buffer with the full bytes
+	c, err := r.Read(p[:n/8])
+	if err != nil {
+		return c, err
+	}
+
+	n %= 8
+	if n > 0 {
+		p[c], err = r.ReadBits8(n)
+		c++
+	}
+
+	return c, err
+}
+
+// Bit reads a single bit from the reader and returns it as a bool (1 == true, 0 == false).
+func (r *Reader) Bit() (bool, error) {
+	b, err := r.ReadBits8(1)
+	return b == 1, err
+}
+
+// BitsBuffered returns the number of bits that are currently buffered in the reader. This value is valid until the next read.
+func (r *Reader) BitsBuffered() uint {
+	return r.len
+}
---
+++ writer.go
@@ -0,0 +1,104 @@
+package bitio
+
+import "io"
+
+// Writer offers support for writes of arbitrary bit length.
+type Writer struct {
+	free uint
+	w    io.Writer
+	bits byte
+}
+
+// NewWriter creates a new bit writer. If w is already a bit writer it will return w.
+func NewWriter(w io.Writer) *Writer {
+	if bw, ok := w.(*Writer); ok {
+		return bw
+	}
+	return &Writer{
+		free: 8,
+		w:    w,
+	}
+}
+
+// Flush writes the remaining buffer into the writer. If the buffer is empty this is a no-op.
+func (w *Writer) Flush() error {
+	if w.free != 8 {
+		return nil
+	}
+	_, err := w.w.Write([]byte{w.bits})
+	w.free = 8
+	return err
+}
+
+// WriteBits8 writes up to 8 bits into the writer. When a full byte is reached in the buffer it is written to the underlying writer.
+func (w *Writer) WriteBits8(b byte, n uint) error {
+	if n > 8 {
+		n = 8
+	}
+
+	var rem uint
+	if n > w.free {
+		rem = n - w.free
+		n = w.free
+	}
+
+	w.free -= n
+	w.bits <<= n
+	w.bits |= (b & (1<<n - 1))
+
+	var err error
+	if w.free == 0 {
+		err = w.Flush()
+	}
+	if rem == 0 {
+		return err
+	}
+
+	b >>= n
+	return w.WriteBits8(b, rem)
+}
+
+func (w *Writer) Write(p []byte) (int, error) {
+	for i, b := range p {
+		err := w.WriteBits8(b, 8)
+		if err != nil {
+			return i, err
+		}
+	}
+	return len(p), nil
+}
+
+// WriteBits writes n bits from p. The function will not write more bits that can be contained in p.
+func (w *Writer) WriteBits(p []byte, n uint) (int, error) {
+	// make sure to only write the appropriate number of bits
+	if maxLen := uint(len(p)) * 8; maxLen < n {
+		n = maxLen
+	}
+
+	c, err := w.Write(p[:n/8])
+	if err != nil {
+		return c, err
+	}
+
+	m := n % 8
+	if m > 0 {
+		err = w.WriteBits8(p[n/8], m)
+		c++
+	}
+
+	return c, err
+}
+
+// Bit writes a single bit into the writer.
+func (w *Writer) Bit(b bool) error {
+	var by byte
+	if b {
+		by = 1
+	}
+	return w.WriteBits8(by, 1)
+}
+
+// BitsBuffered returns the number of bits that are currently buffered in the writer. This value is valid until the next write.
+func (w *Writer) BitsBuffered() uint {
+	return 8 - w.free
+}
